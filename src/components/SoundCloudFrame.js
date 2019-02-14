5d9bdc5e0ad0151d5a27231d4fa69aca033a42f6
alfredosorio <alfred.osorio@outlook.com>
2019-02-14 16:30:03 +1100

    Remove unused assets

--- src/components/SoundCloudFrame.js
+++ src/components/SoundCloudFrame.js
@@ -2,17 +2,15 @@ import React from "react";
 
 const SoundCloudFrame = () => {
   return (
-    <div className="soundcloud">
-      <iframe
-        width="100%"
-        height="123"
-        scrolling="no"
-        frameborder="no"
-        allow="autoplay"
-        title="soundcloudframe"
-        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/554774223&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-      />
-    </div>
+    <iframe
+      width="100%"
+      height="123"
+      scrolling="no"
+      frameBorder="no"
+      allow="autoplay"
+      title="soundcloudframe"
+      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/554774223&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+    />
   );
 };
 
