75bef14850636441b3cf7f1bb629a5c2cc9a292c
mts-developer <itmanagement@mytravelsolution.com.au>
2019-05-24 12:17:34 +1000

    Handle followers goal

--- src/App.js
+++ src/App.js
@@ -44,37 +44,37 @@ class App extends Component {
       );
     }
 
-    const countDownTimer = () => {
-      // Set the date we're counting down to
-      var countDownDate = new Date("May 31, 2019 00:00:00").getTime();
+    // const countDownTimer = () => {
+    //   // Set the date we're counting down to
+    //   var countDownDate = new Date("May 31, 2019 00:00:00").getTime();
 
-      // Update the count down every 1 second
-      var x = setInterval(function() {
-        // Get today's date and time
-        var now = new Date().getTime();
+    //   // Update the count down every 1 second
+    //   var x = setInterval(function() {
+    //     // Get today's date and time
+    //     var now = new Date().getTime();
 
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
+    //     // Find the distance between now and the count down date
+    //     var distance = countDownDate - now;
 
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor(
-          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-        );
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    //     // Time calculations for days, hours, minutes and seconds
+    //     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    //     var hours = Math.floor(
+    //       (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    //     );
+    //     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    //     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        // Display the result in the element with id="timer"
-        document.getElementById("timer").innerHTML =
-          days + "d" + hours + "h" + minutes + "m" + seconds + "s ";
+    //     // Display the result in the element with id="timer"
+    //     document.getElementById("timer").innerHTML =
+    //       days + "d" + hours + "h" + minutes + "m" + seconds + "s ";
 
-        // If the count down is finished, write some text
-        if (distance < 0) {
-          clearInterval(x);
-          document.getElementById("timer").innerHTML = "DEPLOYING...";
-        }
-      }, 1000);
-    };
+    //     // If the count down is finished, write some text
+    //     if (distance < 0) {
+    //       clearInterval(x);
+    //       document.getElementById("timer").innerHTML = "DEPLOYING...";
+    //     }
+    //   }, 1000);
+    // };
 
     const formatNumber = num => {
       var num_parts = num.toString().split(".");
@@ -90,7 +90,10 @@ class App extends Component {
           {/* <h2 id="timer">{countDownTimer()}</h2> */}
           <div style={{ paddingTop: "10px" }}>
             <h3>
-              We're almost at 4K followers on Instagram!
+              {this.state.followers <= 3999
+                ? "We're almost at "
+                : "We've reached over "}
+              4K followers on Instagram!
               <br />
               @o.g_nerd - {formatNumber(this.state.followers)} followers
             </h3>
